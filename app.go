c71952ceeb821a525359f9822c967b90075a3f60
Kumar Gaurav <kumar.gaurav@skyscanner.net>
2020-01-28 17:52:58 +0000

    added variables for static and templates. Also added helper function to pass multiple variables in html templates

--- app.go
+++ app.go
@@ -12,6 +12,9 @@ import (
 
 var mode string
 
+var staticAssetsDir = os.Getenv("STATIC_ASSETS_DIR")
+var templatesDir = os.Getenv("TEMPLATES_DIR")
+
 func main() {
 
 	modePtr := flag.String("mode", "", "which mode to run")
@@ -24,7 +27,7 @@ func main() {
 	if *modePtr == "dev" {
 		mode = "dev"
 	}
-	staticHandler := http.FileServer(http.Dir("static"))
+	staticHandler := http.FileServer(http.Dir(staticAssetsDir))
 
 	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
 	http.HandleFunc("/", indexHandler)
--- utils.go
+++ utils.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"text/template"
 )
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
-	templates := template.Must(template.ParseGlob("templates/*"))
+	templates := template.Must(template.New("").Funcs(template.FuncMap{
+		"dict": func(values ...interface{}) (map[string]interface{}, error) {
+			if len(values)%2 != 0 {
+				return nil, errors.New("invalid dict call")
+			}
+			dict := make(map[string]interface{}, len(values)/2)
+			for i := 0; i < len(values); i += 2 {
+				key, ok := values[i].(string)
+				if !ok {
+					return nil, errors.New("dict keys must be strings")
+				}
+				dict[key] = values[i+1]
+			}
+			return dict, nil
+		},
+	}).ParseGlob(templatesDir + "/*"))
+
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
